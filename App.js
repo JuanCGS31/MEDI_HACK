a8bf75207a25f7b90453416d54a3e91831be8e8e
diangelob <beramirez272gmail.com>
2023-09-08 20:38:48 -0700

    Comenzar Proyecto

--- App.js
+++ App.js
@@ -1,6 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import React from 'react';
+import {Slider} from '@miblanchard/react-native-slider';
+import {AppRegistry, StyleSheet, View, Text} from 'react-native';
+
+class SliderExample extends React.Component {
+    state = {
+        value: 0.2,
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Slider
+                    value={this.state.value}
+                    onValueChange={value => this.setState({value})}
+                />
+                <Text>Value: {this.state.value}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginLeft: 10,
+        marginRight: 10,
+        alignItems: 'stretch',
+        justifyContent: 'center',
+    },
+});
+
+AppRegistry.registerComponent('SliderExample', () => SliderExample);
+
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -10,11 +45,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
