bbb5c7ac85cae82f3c18a19adefa2e19b5fe1aab
diangelob <beramirez272gmail.com>
2023-09-10 13:31:31 -0700

    elgato slider

--- gatito.js
+++ gatito.js
@@ -1,15 +1,62 @@
-import React from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ImageBackground, Image } from 'react-native';
 import Slider from '@react-native-community/slider';
 
 export default function NewPage() {
+  const [sliderValue, setSliderValue] = useState(0);
+
+  // Function to determine which image to display based on sliderValue
+  const getImageForSliderValue = () => {
+    if (sliderValue < 33) {
+      return require('./assets/elgatoBlue.png'); // Replace with the path to your first image
+    } else if (sliderValue < 66) {
+      return require('./assets/elgato.png'); // Replace with the path to your second image
+    } else {
+      return require('./assets/elgatoGreen.png'); // Replace with the path to your third image
+    }
+  };
+
+  // Function to determine the text to display based on sliderValue
+  const getTextForSliderValue = () => {
+    if (sliderValue < 33) {
+      return 'Low Value';
+    } else if (sliderValue < 66) {
+      return 'Medium Value';
+    } else {
+      return 'High Value';
+    }
+  };
+
   return (
     <ImageBackground
       source={require('./assets/Background.png')} // Replace with the path to your background image
       style={styles.background}
     >
       <View style={styles.container}>
-        <Text style={styles.text}>This is the New Page!</Text>
+        <View style={styles.content}>
+          <Text style={styles.text}>This is the New Page!</Text>
+
+          {/* Add the Slider component */}
+          <Slider
+            style={styles.slider}
+            minimumValue={0}
+            maximumValue={100}
+            value={sliderValue}
+            onValueChange={(newValue) => setSliderValue(newValue)}
+          />
+
+          {/* Display the slider value */}
+          <Text style={styles.sliderValue}>Slider Value: {sliderValue.toFixed(2)}</Text>
+
+          {/* Display text based on sliderValue */}
+          <Text style={styles.sliderText}>{getTextForSliderValue()}</Text>
+        </View>
+
+        {/* Display image based on sliderValue */}
+        <Image
+          source={getImageForSliderValue()}
+          style={styles.bottomImage}
+        />
       </View>
     </ImageBackground>
   );
@@ -18,9 +65,14 @@ export default function NewPage() {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    resizeMode: 'contain', // You can adjust this to 'contain' or other values as needed
+    resizeMode: 'cover', // You can adjust this to 'contain' or other values as needed
   },
   container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  content: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -28,5 +80,24 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     color: 'blue',
+  },
+  slider: {
+    width: 200,
+    marginTop: 20,
+  },
+  sliderValue: {
+    fontSize: 18,
+    marginTop: 10,
+    color: 'white',
+  },
+  sliderText: {
+    fontSize: 18,
+    marginTop: 10,
+    color: 'white',
+  },
+  bottomImage: {
+    width: 400, // Adjust the width as needed for the images
+    height: 400, // Adjust the height as needed for the images
+    marginBottom: 20, // Adjust the margin as needed
   },
 });
--- login.js
+++ login.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 export default function AnotherPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // You can add your login logic here
+    // For example, you can send the username and password to a server for authentication
+    // This is a basic example for demonstration purposes
+    if (username === 'yourUsername' && password === 'yourPassword') {
+      alert('Login successful!');
+    } else {
+      alert('Login failed. Please check your credentials.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is the New Page!</Text>
+      <Text style={styles.text}>Login Page</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry // Hide password input
+      />
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
 }
@@ -18,5 +46,13 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     color: 'blue',
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 8,
   },
 });
