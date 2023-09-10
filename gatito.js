28f530023a95c2eef7852b27e4344674c11fed87
diangelob <beramirez272gmail.com>
2023-09-10 13:07:31 -0700

    backgrond gatito

--- gatito.js
+++ gatito.js
@@ -1,20 +1,25 @@
-//aqui va el gatito
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 import Slider from '@react-native-community/slider';
 
-
-
 export default function NewPage() {
-    const [sliderValue, setSliderValue] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is the New Page!</Text>
-    </View>
+    <ImageBackground
+      source={require('./assets/Background.png')} // Replace with the path to your background image
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Text style={styles.text}>This is the New Page!</Text>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'contain', // You can adjust this to 'contain' or other values as needed
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
